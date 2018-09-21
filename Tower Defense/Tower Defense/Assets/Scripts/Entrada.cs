882725dd02b716f2ef07c73779aaac697eda30a1
Gustavo <gustavosr98@gmail.com>
2018-09-21 18:03:36 -0400

    WaveSpawner Basico
    - En Entrada.cs e instancia un tipo de enemigo cada segundo haciendo una llamada a un metodo cada X tiempo.
    InvokeRepeatint("metodo", tiempo de espera, tiempo entre cada llamada)

---
+++ Tower Defense/Tower Defense/Assets/Scripts/Entrada.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entrada : MonoBehaviour {
+
+    [SerializeField] GameObject[] enemies;
+    int enemyIndex = 0;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnEnemy", 0f, 1f);
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemies[enemyIndex], transform.position, transform.rotation);
+        enemyIndex++;
+        if (enemyIndex > enemies.Length - 1)
+            enemyIndex = 0;
+    }
+}
